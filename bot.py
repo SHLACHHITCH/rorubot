8506695fb0a7ca8d307ecf4abf6b2ce20fa06014
SHLACHHITCH <56740690+SHLACHHITCH@users.noreply.github.com>
2020-07-05 16:23:06 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -29,7 +29,10 @@ async def status(ctx):
     await ctx.send(embed=emb)
 #/////////////////////////////// role giver ///////////////////////#
 
-
+@Bot.event
+async def on_member_join(member):
+    role = discord.utils.get(member.server.roles, id=<719459081492103218>)
+    await bot.add_roles(member, role)
 
 #/////////////////////////////// server welcomer ///////////////////////#
 
